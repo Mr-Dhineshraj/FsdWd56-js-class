62d630b6b70baa0fd4903398679face91e3a100c
DhineshrajBaskaran <dhineshrajbaskaran01@gmail.com>
2024-03-03 11:11:22 +0530

    callback  Stirng Function

--- day16/src/index.js
+++ day16/src/index.js
@@ -1,23 +1,37 @@
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
+
+// }
+
+// function sub(a,b){
+//     return a-b;
+// }
+
+// function multi(a,b){
+//     return a*b;
+
+// }
+// function divide(a,b){
+//     return a/b;
+// }
+
+// function calc(a,b,callback){
+//     return callback(a,b)
+
+// }
+
+// console.log(calc(10,5,add))
+
+
+
+function cb(word){
+    console.log(word);
 
 }
 
-function sub(a,b){
-    return a-b;
+function repeatStirng(a,callback){
+    console.log(a);
+    callback(a);
 }
 
-function multi(a,b){
-    return a*b;
-
-}
-function divide(a,b){
-    return a/b;
-}
-
-function calc(a,b,callback){
-    return callback(a,b)
-
-}
-
-console.log(calc(10,5,add))
+repeatStirng("Hello",cb)
